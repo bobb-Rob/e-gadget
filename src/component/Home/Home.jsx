4f8273ef29ababe791712c16bbd91f8116ef6184
Robertson Akpan <robertsonakpan@gmail.com>
2022-06-13 18:35:20 +0100

    gh-pages installed

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,35 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {MdArrowForward} from 'react-icons/md';
-import './Home.css'
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { MdArrowForward } from "react-icons/md";
+import "./Home.css";
 
 const Home = () => {
-    
   return (
-        <main 
-        style={{marginTop: '120px'}}
-        >           
-            <h1 className='home-header' >
-                Find all sorts of gadgets you'll love. Support independent sellers. <br /> Only on Gadgette.
-            </h1>
-            <Link to={'shop'} >
-                <button className='see-product-btn'>
-                    <div>All products</div> 
-                    <MdArrowForward className='forward-btn' />
-                </button>
-            </Link>
-            
-             {/* <div className='image-background' >
-                <div className='hot-deal-section' >
-                {'man'
-                }
-                
-                </div>             
-             </div>            */}
-        </main>
-    );
+    <main style={{ marginTop: "120px" }}>
+      <h1 className="home-header">
+        Find all sorts of gadgets you'll love. Support independent sellers.{" "}
+        <br /> Only on Gadgette.
+      </h1>
+      <Link to={"shop"}>
+        <button className="see-product-btn">
+          <div>All products</div>
+          <MdArrowForward className="forward-btn" />
+        </button>
+      </Link>
+    </main>
+  );
 };
 
 export default Home;
